939fffab90958d599a789c2f3e78974e58328f37
mahan <mahanbiabani12@gmail.com>
2026-08-03 20:34:32 +0330

    front end plugin registory file

---
+++ src/pluginRegistry.jsx
@@ -0,0 +1,9 @@
+import MusicWindow from './components/MusicWindow';
+import KasaWindow from './components/KasaWindow';
+import PrinterWindow from './components/PrinterWindow';
+
+export const UI_PLUGINS = {
+    music: MusicWindow,
+    kasa: KasaWindow,
+    printer: PrinterWindow,
+};
