cfdbe41a0ab5a2b6cfdebccd6143dd745cfe18b4
mahan <mahanbiabani12@gmail.com>
2026-08-04 07:13:24 +0330

    Plugin registry updated to support all plugins

--- src/pluginRegistry.jsx
+++ src/pluginRegistry.jsx
@@ -1,9 +1,19 @@
 import MusicWindow from './components/MusicWindow';
+import CadWindow from './components/CadWindow';
 import KasaWindow from './components/KasaWindow';
+import DoorWindow from './components/DoorWindow';
 import PrinterWindow from './components/PrinterWindow';
+import BrowserWindow from './components/BrowserWindow';
+import ChessWindow from './components/ChessWindow';
 
+// Registry of all available UI plugins
 export const UI_PLUGINS = {
     music: MusicWindow,
+    cad: CadWindow,
     kasa: KasaWindow,
+    door: DoorWindow,
     printer: PrinterWindow,
+    browser: BrowserWindow,
+    chess: ChessWindow,
+    // Add new plugins here...
 };
